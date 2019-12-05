919ccadf80daac7d5fe180f3a5ee0afdf9e74bf9
florine4444 <58477543+florine4444@users.noreply.github.com>
2019-12-05 23:19:32 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Mandelgetal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(119, 82);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(196, 26);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "midden X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "midden Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(339, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "schaal:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(339, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "max:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(404, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(404, 82);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(99, 26);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+            this.button1.Location = new System.Drawing.Point(525, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "basis",
+            "ster",
+            "vuur",
+            "salamander"});
+            this.listBox1.Location = new System.Drawing.Point(640, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(88, 84);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(34, 147);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(687, 687);
+            this.panel1.TabIndex = 12;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(775, 874);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mandelgetal
+{
+    public partial class Form1 : Form
+    {
+        //declareert de variabelen die we nodig gaan hebben
+        int t;
+        double invoerX, invoerY, max, schaal, afstand, a, b, olda, min, nieuwX, nieuwY, geschaaldeX, geschaaldeY;
+        //declareert de standaardkleuren van onze mandelbrot
+        Color kleur1 = Color.Black;
+        Color kleur2 = Color.White;
+        Color kleur3 = Color.Black;
+        Color kleur4 = Color.White;
+        Color kleur5 = Color.Black;
+
+
+        //creëert de voorbeeldplaatjes
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //deze variabele neemt de waarde van het geselecteerde item aan
+            var itemstring = (string)listBox1.SelectedItem;
+            //door middel van if-statements checken we welke waarde de variabele heeft gekregen en worden de voorbeeldplaatjes gegenereerd
+            if (itemstring == "vuur")
+            {
+                textBox1.Text = "-324";
+                textBox3.Text = "3080";
+                textBox2.Text = "0,0003125";
+                textBox4.Text = "100";
+                kleur1 = Color.Red;
+                kleur2 = Color.Orange;
+                kleur3 = Color.DarkOrange;
+                kleur4 = Color.SandyBrown;
+                kleur5 = Color.OrangeRed;
+            }
+            if (itemstring == "ster")
+            {
+                textBox1.Text = "-361768";
+                textBox3.Text = "-8530";
+                textBox2.Text = "4,8828125E-06";
+                textBox4.Text = "100";
+                kleur1 = Color.White;
+                kleur2 = Color.LightYellow;
+                kleur3 = Color.Wheat;
+                kleur4 = Color.DarkOrange;
+                kleur5 = Color.DarkOrange;
+            }
+            if (itemstring == "salamander")
+            {
+                textBox1.Text = "-35006";
+                textBox3.Text = "-170942";
+                textBox2.Text = "4,8828125E-06";
+                textBox4.Text = "100";
+                kleur1 = Color.Blue;
+                kleur2 = Color.ForestGreen;
+                kleur3 = Color.DarkGreen;
+                kleur4 = Color.DarkSeaGreen;
+                kleur5 = Color.DarkOliveGreen;
+            }
+            if (itemstring == "basis")
+            {
+                textBox1.Text = "0";
+                textBox3.Text = "0";
+                textBox2.Text = "0,01";
+                textBox4.Text = "100";
+                kleur1 = Color.Black;
+                kleur2 = Color.White;
+                kleur3 = Color.Black;
+                kleur4 = Color.White;
+                kleur5 = Color.Black;
+            }
+            //panel 1 wordt opnieuw getekend zodat het voorbeeldplaatje verschijnt
+            panel1.Invalidate();
+        }
+
+        //regelt wat er gebeurt als de gebruiker op het plaatje klikt
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //x en y van het aangeklikte punt worden gebruikt om het nieuwe middelpunt te bepalen
+            //hierbij wordt rekening gehouden met waar het nulpunt van panel 1 ligt
+            textBox1.Text = ((int.Parse(textBox1.Text) + (e.X - panel1.Width / 2)) * 2).ToString();
+            textBox3.Text = ((int.Parse(textBox3.Text) + (e.Y - panel1.Height / 2)) * 2).ToString();
+            //de schaal wordt twee keer zo klein
+            textBox2.Text = (schaal / 2).ToString();
+            //panel 1 wordt opnieuw getekend
+            panel1.Invalidate();
+        }
+
+     
+        public Form1()
+        {
+            InitializeComponent();
+            //geeft de textboxen beginwaardes
+            textBox1.Text = "0";
+            textBox2.Text = "0,01";
+            textBox3.Text = "0";
+            textBox4.Text = "100";
+            //bepaalt de grootte van het form
+            this.ClientSize = new Size(505, 560);
+
+        }
+        //regelt wat er gebeurt als er op de OK knop wordt gedrukt
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //panel 1 wordt opnieuw getekend
+            panel1.Invalidate();
+        }
+
+        //berekent de schaal die we nodig hebben voor het tekenen van de mandelbrot
+        private void Schaalberekening(double schaal)
+        {
+            var uitkomst = schaal * (panel1.Width / 2);
+            min = 0 - uitkomst;
+        }
+        //hiermee gaan we de geschaalde x en y coördinaat berekenen die in de functie moeten
+        private double GeschaaldeXY(double xy)
+        {
+            return min + (schaal * xy);
+        }
+
+        //regelt het tekenen van de Mandelbrot-figuur in het panel
+        private void panel1_Paint(object sender, PaintEventArgs pea)
+        {
+            //de invoer van de tekstboxen worden omgezet naar variabelen
+            invoerX = double.Parse(textBox1.Text);
+            invoerY = double.Parse(textBox3.Text);
+            schaal = double.Parse(textBox2.Text);
+            max = double.Parse(textBox4.Text);
+            //methode wordt opgeroepen om de schaal te berekenen
+            Schaalberekening(schaal);
+
+            //voor elke pixel op de y-as, voer de while-loop uit zodat de pixel getekend wordt
+            for (var huidigY = 0; huidigY <= panel1.Height; huidigY++)
+            {
+                //voor elke pixel op de x-as, voer de while-loop uit zodat de pixel getekend wordt
+                for (var huidigX = 0; huidigX <= panel1.Width; huidigX++)
+                {
+                    //reset de afstand, teller, a en b naar 0 vóór elke while-loop
+                    a = 0;
+                    b = 0;
+                    afstand = 0;
+                    t = 0;
+                    //berekent de geschaalde x en y coördinaat
+                    nieuwX = huidigX + invoerX;
+                    nieuwY = huidigY + invoerY;
+                    geschaaldeX = GeschaaldeXY(nieuwX);
+                    geschaaldeY = GeschaaldeXY(nieuwY);
+
+                    //hier gaan we de functie toepassen
+                    //dit gebeurt alleen zolang de afstand onder 2 is en het aantal herhalingen van de functie onder de max is
+                    while (afstand <= 2 && t < max)
+                    {
+                        //we slaan de oude waarde van 'a' op zodat er niet met de nieuwe waarde wordt gerekend in de formule voor 'b'
+                        olda = a;
+                        t++;
+                        a = a * a - b * b + geschaaldeX;
+                        b = 2 * olda * b + geschaaldeY;
+                        afstand = Math.Sqrt(a * a + b * b);
+
+                    }
+
+                    //tekent de pixels
+                    //voor elk standaardplaatje worden er andere kleuren gebruikt dmv de kleur variabelen
+                    if (t % 5 == 0)
+                    {
+                        pea.Graphics.FillRectangle(new SolidBrush(kleur1), huidigX, huidigY, 1, 1);
+                    }
+                    else if (t % 4 == 0)
+                    {
+                        pea.Graphics.FillRectangle(new SolidBrush(kleur2), huidigX, huidigY, 1, 1);
+                    }
+                    else if (t % 3 == 0)
+                    {
+                        pea.Graphics.FillRectangle(new SolidBrush(kleur3), huidigX, huidigY, 1, 1);
+                    }
+                    else if (t % 2 == 0)
+                    {
+                        pea.Graphics.FillRectangle(new SolidBrush(kleur4), huidigX, huidigY, 1, 1);
+                    }
+                    else if (t % 1 == 0)
+                    {
+                        pea.Graphics.FillRectangle(new SolidBrush(kleur5), huidigX, huidigY, 1, 1);
+                    }
+
+                }
+            }
+        }
+
+    }
+
+}
